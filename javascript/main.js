2eee02395f80f55fcfc01c894705ec0c6a7babf5
kingryan9996 <kingryan9996@gmail.com>
2022-12-01 01:58:36 +0900

    12.01

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,3 @@
-console.log('안녕하세요');
-
 const apple = document.getElementById("company-info");
 
 let mouseover = () => {
@@ -20,6 +18,9 @@ apple.addEventListener("mouseout",mouseout);
 let hide = () => {
     // console.log(window.pageYOffset)
     let hoc = document.getElementsByTagName("header")[0];
+    let qtn = document.getElementsByClassName("to-top")[0];
+
+    // let totn = document.getElementsByClassName("to-top");
     let i = window.pageYOffset;
     // Math.floor(
     //     document.querySelector("body").scrollHeight        
@@ -32,7 +33,11 @@ let hide = () => {
     // i>0?(hoc.style.add = border:5px solid yellow:false
     // console.log(i>0?true:false);
     i>0?hoc.style.backgroundColor = '#000':hoc.style.backgroundColor = 'transparent';
+    i>400?qtn.style.opacity = 1:qtn.style.opacity = 0;
+    // i>500?topBtn.style.opacity = '1' :topBtn.style.opacity = '0';
+    
 }
+
 
 // window.innerHeight>0?.style = "background-color:#000;":false;
 
