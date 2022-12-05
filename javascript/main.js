2a8d61936cbb90987a1debca6b83e7b23eee13d6
kingryan9996 <89970039+kingryan9996@users.noreply.github.com>
2022-12-05 18:28:15 +0900

    last

--- javascript/main.js
+++ javascript/main.js
@@ -1,36 +1,55 @@
 let hide = () => {
-    // console.log(window.pageYOffset)
-    let hoc = document.getElementsByTagName("header")[0];
-    let qtn = document.getElementsByClassName("to-top")[0];
+  // console.log(window.pageYOffset)
+  let hoc = document.getElementsByTagName("header")[0];
+  let qtn = document.getElementsByClassName("to-top")[0];
 
-    // let totn = document.getElementsByClassName("to-top");
-    let i = window.pageYOffset;
-    // Math.floor(
-    //     document.querySelector("body").scrollHeight        
-    // )
-    // console.log(i);
-    // ((document.querySelector("body").scrollTop /
-    //     (document.querySelector("body").scrollHeight - 
-    //         document.querySelector("body").clientHeight)) * 100);
-            
-    // i>0?(hoc.style.add = border:5px solid yellow:false
-    // console.log(i>0?true:false);
-    i>0?hoc.style.backgroundColor = '#000':hoc.style.backgroundColor = 'transparent';
-    i>400?qtn.style.opacity = 1:qtn.style.opacity = 0;
-    // i>500?topBtn.style.opacity = '1' :topBtn.style.opacity = '0';
-    
-}
+  // let totn = document.getElementsByClassName("to-top");
+  let i = window.pageYOffset;
+  // Math.floor(
+  //     document.querySelector("body").scrollHeight
+  // )
+  // console.log(i);
+  // ((document.querySelector("body").scrollTop /
+  //     (document.querySelector("body").scrollHeight -
+  //         document.querySelector("body").clientHeight)) * 100);
 
+  // i>0?(hoc.style.add = border:5px solid yellow:false
+  // console.log(i>0?true:false);
+  i > 0
+    ? (hoc.style.backgroundColor = "#000")
+    : (hoc.style.backgroundColor = "transparent");
+  i > 400 ? (qtn.style.opacity = 1) : (qtn.style.opacity = 0);
+  // i>500?topBtn.style.opacity = '1' :topBtn.style.opacity = '0';
+};
 
 // window.innerHeight>0?.style = "background-color:#000;":false;
 
-window.addEventListener('scroll',hide);
+window.addEventListener("scroll", hide);
 
+const blueBar = document.querySelector(".swiper-container01");
 
+let resizeTimer = null;
+function resizeComplete() {
+  // 리사이즈 함수 사용
+  if (blueBar.classList.contains("on")) {
+    blueBar.classList.remove("on");
+  } else {
+    blueBar.classList.add("on");
 
+    // blueBar.classList.add('on')
+  }
+}
 
+window.addEventListener("resize", function () {
+    
+  clearTimeout(resizeTimer);
+  blueBar.classList.remove("on");
+  resizeTimer = setTimeout(function () {
+    
+    
+      blueBar.classList.add("on");
 
-
-// let player = (e) => {
-//     lia.innerHTML = "<div>adwwe</div>";
-// }
+      // blueBar.classList.add('on')
+    
+  }, 100);
+});
