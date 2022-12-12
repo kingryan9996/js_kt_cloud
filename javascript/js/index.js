8129161b4c33a60a22c5b5c2f5b4fa1c1489debf
kingryan9996 <89970039+kingryan9996@users.noreply.github.com>
2022-12-12 16:59:25 +0900

    modify:ajax

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,8 +1,30 @@
 $("body").prepend("<header>");
 $("body").append("<footer>");
 
-$("header").load("./html/inc.html header");
+$("header").load("./html/inc.html header",head);
 $("footer").load("./html/inc.html footer");
+$("header").load("../html/inc.html header",head);
+$("footer").load("../html/inc.html footer");
+
+let idx = localStorage.idx || 0;
+
+function head () {
+    $("header li").eq(idx).css("scale","7.0")
+    .css(    'transform','translateY(70px)')
+    .css('background-color','rgba(0,0,0,0.6)')
+    $("header li a").eq(idx).css("color","coral")
+    
+
+    $("header li").click(function(){
+        let idx = $(this).index();
+
+        localStorage.idx = idx;
+    })
+
+
+}
+head();
+
 
 // function head() {
 //   $("header a").click(function () {
